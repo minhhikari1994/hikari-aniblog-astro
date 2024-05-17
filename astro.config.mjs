60611c5020747815bc22ab01813a164fcc6d68a0
Minh Hikari <minhhikari1994+dev@outlook.com>
2024-05-17 17:59:03 +0700

    Add MDX, Tailwind and TVSeriesEntry component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server"
+  output: "server",
+  integrations: [mdx(), tailwind()]
 });
